8a340d84bfed6664ab065c1303687115ff3fb61f
ClaudioAnzini <claudioanzinineto123@gmail.com>
2023-12-07 09:30:46 -0300

    123321

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let operacaoAtual = '';
-let resultadoAtual = '';
+let operacaoAtual = [];
+let resultadoAtual = [];
 
 function atualizarTela() {
-    document.getElementById('ver-operacao').innerText = operacaoAtual;
-    document.getElementById('operacao').innerText = resultadoAtual;
+    document.getElementById('operacao').innerText = conta;
+    document.getElementById('ver-operacao').innerText = resultadoAtual;
 }
 
 function adicionarNumero(numero) {
@@ -12,26 +12,27 @@ function adicionarNumero(numero) {
 }
 
 function adicionarConta(operador) {
-    if (operacaoAtual !== '*' && operacaoAtual !== '+' && operacaoAtual !== '/') {
+    if (operacaoAtual !== "")  {
         operacaoAtual += operador;
         atualizarTela();
     }
 }
 
 function adicionarPonto() {
-    operacaoAtual += '.';
+    operacaoAtual += ".";
     atualizarTela();
 }
 
 function apagarNumeroErrado() {
-    operacaoAtual = '';
+    operacaoAtual = "";
     atualizarTela();
 }
 
 function apagarTudo() {
-    operacaoAtual = '';
-    resultadoAtual = '';
+    operacaoAtual = "";
+    resultadoAtual = "";
     atualizarTela();
+    document.getElementById('btn-igual').focus();
 }
 
 function apagarUltimo() {
@@ -39,11 +40,67 @@ function apagarUltimo() {
     atualizarTela();
 }
 
+let conta = operacaoAtual;
+
 function calcularResultado() {
-    
         resultadoAtual = eval(operacaoAtual);
         operacaoAtual = resultadoAtual.toString();
         atualizarTela();
 
 }
 
+document.addEventListener("keypress", (e) => {
+    if (e.key == "1") {
+        adicionarNumero("1");
+    }
+    if (e.key == "2") {
+        adicionarNumero("2");
+    }
+    if (e.key == "3") {
+        adicionarNumero("3");
+    }
+    if (e.key == "4") {
+        adicionarNumero("4");
+    }
+    if (e.key == "5") {
+        adicionarNumero("5");
+    }
+    if (e.key == "6") {
+        adicionarNumero("6");
+    }
+    if (e.key == "7") {
+        adicionarNumero("7");
+    }
+    if (e.key == "8") {
+        adicionarNumero("8");
+    }
+    if (e.key == "9") {
+        adicionarNumero("9");
+    }
+    if (e.key == "0") {
+        adicionarNumero("0");
+    }
+    if (e.key == ".") {
+        adicionarPonto(".");
+    }
+    if (e.key == "Enter") {
+        calcularResultado();
+    }
+    if (e.key == "Backspace") {
+        apagarNumeroErrado();
+    }
+    if (e.key == "+") {
+        adicionarNumero("+");
+    }
+    if (e.key == "-") {
+        adicionarNumero("-");
+    }
+    if (e.key == "/") {
+        adicionarNumero("/");
+    }
+    if (e.key == "*") {
+        adicionarNumero("*");
+    }
+
+    console.log(e.key)
+})
